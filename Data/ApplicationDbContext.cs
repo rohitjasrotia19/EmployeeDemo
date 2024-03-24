92b3abcd1fa401f199cd53dbbc62459108f9257d
DESKTOP-DFT1TDC\Rohit <adarshjasrotia14@gmail.com>
2024-03-24 12:04:37 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
